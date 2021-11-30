0aa3df5cabd5d89e387b8ed374ceb15dccd7a870
BEDongryeol <ehdfuf53@gmail.com>
2021-11-30 20:43:37 +0900

    Refactor: Delete unnecessary modifiers

--- src/main/java/com/fastcampus/component/service/PostService.java
+++ src/main/java/com/fastcampus/component/service/PostService.java
@@ -10,9 +10,9 @@ public interface PostService {
     // 게시글 생성
     void createPost(PostVO vo);
 
-    public PostVO getPost(PostVO vo);
+    PostVO getPost(PostVO vo);
 
-    public List<PostVO> getPosts(PostVO vo);
+    List<PostVO> getPosts(PostVO vo);
 
     // 게시글 수정
     void updatePost(PostVO vo);
