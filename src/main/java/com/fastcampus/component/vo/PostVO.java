bb3a61e676caf0f7bc8063374a4b851546ddc1bb
BEDongryeol <ehdfuf53@gmail.com>
2021-11-30 20:45:38 +0900

    feat: Make PostVO Entity
    1. To be used with JPA, Added Annotations
    2. To initialize Date, made a hidden builder

--- src/main/java/com/fastcampus/component/vo/PostVO.java
+++ src/main/java/com/fastcampus/component/vo/PostVO.java
@@ -2,13 +2,44 @@ package com.fastcampus.component.vo;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.Date;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(builderMethodName = "hiddenBuilder")
+@Entity
+@Table(name = "POST")
 public class PostVO {
-    private int postId;
-    private int categoryId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer postId;
+
+    @Column(nullable = false)
+    private Integer categoryId;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String content;
+
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Builder.Default
     private Date createdDate = new Date();
+
+    public static PostVOBuilder builder(String title, String content){
+        return hiddenBuilder()
+                .title(title)
+                .content(content);
+    }
+
+
+
 }
